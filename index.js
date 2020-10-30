103968b3db7157917c4fc2bc465716a8d753a396
gokulnt2008 <70792971+gokulnt2008@users.noreply.github.com>
2020-10-30 11:35:34 +0530

    first Commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const urlParams = new URLSearchParams(window.location.search);
+const myParam = urlParams.get('name');
+
+
+
+document.getElementById('bg').innerHTML = myParam + "<br> <br>" + "Wishing You A Very Happy Diwali" + "<br><br>" + "<img class=gif src=https://im6.ezgif.com/tmp/ezgif-6-13a1085f983b.gif>" + "<div class=footer><input id=input type=Text><button onclick=share()>Share On Whatsapp</button></div>"
+
+
+function share() { 
+
+    const form = document.getElementById('input')
+const name = `file:///F:/Gokul/clickme%20clone/index.html?name=${form.value }`
+console.log(form.value)
+  
+    // Getting user input 
+    var message = $("input[name=message]").val(); 
+
+    // Opening URL 
+    window.open( 
+        "whatsapp://send?text=" + name, 
+
+        // This is what makes it  
+        // open in a new window. 
+        '_blank'  
+    ); 
+} 
